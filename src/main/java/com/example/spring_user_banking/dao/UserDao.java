e27b860e182fe14882824b13467bc017225d8318
ilyaviduk <vidukilya@gmail.com>
2025-04-07 21:24:18 +0300

    feat(dao): create UserDao interface

---
+++ src/main/java/com/example/spring_user_banking/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.example.spring_user_banking.dao;
+
+import com.example.spring_user_banking.model.User;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    Optional<User> findById(Long id);
+    List<User> findAll();
+    boolean update(User user);
+
+    boolean addEmail(Long userId, String email);
+    boolean removeEmail(Long userId, String email);
+    boolean addPhone(Long userId, String phone);
+    boolean removePhone(Long userId, String phone);
+
+    List<User> findByNameStartingWith(String namePrefix);
+    List<User> findByBirthDateAfter(LocalDate date);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+}
