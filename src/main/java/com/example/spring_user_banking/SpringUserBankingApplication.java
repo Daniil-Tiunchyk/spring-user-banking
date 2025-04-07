2865a2374346bb109d35bd892606d9a281a9bef7
Daniil-Tiunchyk <90754791+Daniil-Tiunchyk@users.noreply.github.com>
2025-04-07 17:07:46 +0300

    init: set up base REST API project with Swagger integration

---
+++ src/main/java/com/example/spring_user_banking/SpringUserBankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_user_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_user_banking/SpringUserBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_user_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
