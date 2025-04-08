690401d3383aaf388f07c7ed2e2ecbd43a3b297f
Daniil-Tiunchyk <90754791+Daniil-Tiunchyk@users.noreply.github.com>
2025-04-08 16:01:05 +0300

    [feat/application-layer] feat: integrate Swagger authentication and refine security filter

--- src/main/java/com/example/spring_user_banking/config/SwaggerConfig.java
+++ src/main/java/com/example/spring_user_banking/config/SwaggerConfig.java
@@ -4,22 +4,56 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String AUTH_SCHEME_NAME = "JWT";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 // Указываем, где искать контроллеры (например, пакет с REST-контроллерами)
                 .apis(RequestHandlerSelectors.basePackage("com.example.spring_user_banking"))
-                // Или можно указать .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(List.of(securityContext()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey(AUTH_SCHEME_NAME, AUTH_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(AUTH_SCHEME_NAME, new AuthorizationScope[]{authorizationScope}));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("User Banking API")
+                .description("Документация к REST API для user-banking")
+                .version("1.0")
                 .build();
     }
 }
