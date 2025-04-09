0a2c56914139391319531362c348e7028639da49
Daniil-Tiunchyk <90754791+Daniil-Tiunchyk@users.noreply.github.com>
2025-04-09 13:18:10 +0300

    [feat/banking-ops] chore: update Flyway migration scripts for data initialization

--- src/main/resources/db/migration/V1__create_users.sql
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -1,7 +1,8 @@
+-- Создание таблицы пользователей
 CREATE TABLE IF NOT EXISTS users
 (
     id            BIGSERIAL PRIMARY KEY,
     name          VARCHAR(500) NOT NULL,
     date_of_birth DATE,
-    password VARCHAR(255)
+    password      VARCHAR(255)
 );
--- src/main/resources/db/migration/V5__insert_initial_data.sql
+++ src/main/resources/db/migration/V5__insert_initial_data.sql
@@ -2,24 +2,38 @@
 
 -- Вставляем данные пользователей
 INSERT INTO users (name, date_of_birth, password)
-VALUES ('Alice', '1990-01-15', 'hashed_password1'),
-       ('Bob', '1985-05-20', 'hashed_password2'),
-       ('Charlie', '1978-12-09', 'hashed_password3');
+VALUES ('Alice', '1990-01-15', 'user1'),
+       ('Bob', '1985-05-20', 'user2'),
+       ('Charlie', '1978-12-09', 'user3'),
+       ('David', '1992-03-23', 'user4'),
+       ('Eva', '1988-07-14', 'user5');
 
--- Вставляем данные аккаунтов
+-- Вставляем данные аккаунтов с указанием начального баланса
 INSERT INTO account (user_id, balance)
 VALUES (1, 1500.50),
        (2, 2350.75),
-       (3, 320.00);
+       (3, 320.00),
+       (4, 800.00),
+       (5, 1250.00);
 
--- Вставляем данные email
+-- Вставляем данные email для каждого пользователя (для некоторых более одного)
 INSERT INTO email_data (user_id, email)
-VALUES (1, 'alice@example.com'),
+VALUES (1, 'alice.primary@example.com'),
+       (1, 'alice.secondary@example.com'),
        (2, 'bob@example.com'),
-       (3, 'charlie@example.com');
+       (3, 'charlie@example.com'),
+       (3, 'charlie.alt@example.com'),
+       (4, 'david@example.com'),
+       (5, 'eva@example.com'),
+       (5, 'eva.work@example.com');
 
--- Вставляем данные телефонов
+-- Вставляем данные телефонов для каждого пользователя (для некоторых более одного)
 INSERT INTO phone_data (user_id, phone)
 VALUES (1, '+12345678901'),
+       (1, '+12345678902'),
        (2, '+19876543210'),
-       (3, '+10987654321');
+       (3, '+10987654321'),
+       (3, '+10987654322'),
+       (4, '+11223344556'),
+       (5, '+12223334455'),
+       (5, '+12223334456');
